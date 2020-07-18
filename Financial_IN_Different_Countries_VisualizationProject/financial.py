218f0a6b67dd96f7d5e68dd90cb536f48b027961
rrtyagii <rtyagi1@hawk.iit.edu>
2020-07-17 22:10:01 -0500

    Further commit

--- Financial_IN_Different_Countries_VisualizationProject/financial.py
+++ Financial_IN_Different_Countries_VisualizationProject/financial.py
@@ -3,9 +3,22 @@ import matplotlib.pyplot as pyplot
 import matplotlib as mat
 import seaborn as sns
 
-data=pandas.read_excel("C:\\IIT\\Python Data Science\\Financial_IN_Different_Countries_VisualizationProject\\global_crisis_data_20160923.xlsx", sheet_name="Sheet1")
-data.drop( ["exch_usd_alt1","exch_usd_alt2", "exch_usd_alt3","Domestic_Debt_ Notes/Sources", "Defaults_External_Notes","conversion_notes", "Banking_Crisis_Notes"], axis=1, inplace=True )
-#print(data.info() )
+data=pandas.read_excel("C:\\IIT\\Python Data Science\\Financial_IN_Different_Countries_VisualizationProject\\global_crisis_data_20160923(2).xlsx", sheet_name="Sheet1")
+data.drop( ["exch_usd_alt1","exch_usd_alt2","<", "exch_usd_alt3","Domestic_Debt_ Notes/Sources", "Defaults_External_Notes","conversion_notes", "Banking_Crisis_Notes"], axis=1, inplace=True )
+
 countries,years,cc3  = (data.Country.unique()[1:]),  (data.Year.unique()[1:]), (data.CC3.unique()[1:])
-print(countries)
-print(cc3)
+
+print(data.columns)
+print()
+# pyplot.figure(figsize=(15,5))
+# ax= sns.scatterplot(x=data['Country'], y=data['Year'], data=data['Independence'])
+
+# pyplot.xticks(
+#     rotation=90,
+#     horizontalalignment='right',
+#     fontweight='light',
+#     fontsize='x-large'
+
+# )
+
+pyplot.show()
---
+++ Logistic/logistic.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pyplot
+import seaborn as seaborn
+
+train = pd.read_csv("C:\\IIT\\Python Data Science\\Logistic\\titanic_train.csv")
+print(train.isnull())
+
+
+
+pyplot.show()
